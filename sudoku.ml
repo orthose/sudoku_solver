7fe360c5ea0d40ffafbafe50584bbf09f534f65c
maxime vincent <maxime.vincent1@u-psud.fr>
2021-05-30 19:52:46 +0200

    Simplification print_sudoku

--- sudoku.ml
+++ sudoku.ml
@@ -1,3 +1,4 @@
+(* Exemple de grille *)
 let grid1 = [|
     [|Some 9;None;None;Some 1;None;None;None;None;Some 5|];
     [|None;None;Some 5;None;Some 9;None;Some 2;None;Some 1|];
@@ -10,9 +11,11 @@ let grid1 = [|
     [|None;None;Some 1;Some 9;None;Some 4;Some 5;Some 7;None|]
 |]
 
+(* Vérifie que le sudoku est une matrice 9*9 *)
 let is_sudoku grid =
     Array.length grid = 9 && Array.length grid.(0) = 9
-    
+
+(* Parsing de fichier de grille de sudoku et renvoie la matrice *)   
 let parse file =
     let cursor = open_in file in
     let res = Array.make_matrix 9 9 None in
@@ -31,16 +34,15 @@ let parse file =
                 ) line in fill (i + 1)
         with End_of_file -> close_in cursor
     in let () = fill 0 in res
-    
-let print_sudoku grid =
+
+(* Affichage de sudoku en console *)
+let print_sudoku =
     let print x = Printf.printf "%c" (match x with
         | None -> '?'
         | Some y -> (string_of_int y).[0]
         )
     in
-    let rec print_sudoku i j =
-        if i < 9 then
-            if j < 9 then (print grid.(i).(j); print_sudoku i (j + 1))
-            else (print_endline ""; print_sudoku (i + 1) 0)
-    in print_sudoku 0 0
+    Array.iter (fun a -> 
+        Array.iter (fun x -> print x) a; 
+        print_endline "")
     
