c140bbbaa952e57ebe37438d42d4e076d11cc2ca
maxime vincent <maxime.vincent1@u-psud.fr>
2021-05-31 11:40:09 +0200

    Amélioration du parsing avec gestion erreurs et ajout d'espaces pour meilleure visualisation dans fichier et console

--- sudoku.ml
+++ sudoku.ml
@@ -1,3 +1,9 @@
+(**************************************************)
+(* Le module Sudoku contient toutes les fonctions *)
+(* de manipulation de sudoku avec des fichiers ou *)
+(* via la console et l'entrée/sortie standard     *)
+(**************************************************)
+
 (* Exemple de grille *)
 let grid1 = [|
     [|Some 9;None;None;Some 1;None;None;None;None;Some 5|];
@@ -19,30 +25,50 @@ let is_sudoku grid =
 let parse file =
     let cursor = open_in file in
     let res = Array.make_matrix 9 9 None in
+    
+    (* Partial mapping avec f : string -> string *)
+    let map_str f s =
+        let n = String.length s in
+        let rec map_str i acc =
+            if i < n then 
+                map_str (i + 1) (acc ^ (f (String.make 1 s.[i])))
+            else acc 
+        in map_str 0 ""
+    in
+    
+    (* Parcours de toutes les lignes du fichier *)
     let rec fill i =
         if i < 9 then        
         try
-            let line = input_line cursor in
-            if String.length line <> 9 then
-                failwith "Parsing error inconsistent format"
-            else let () = 
+            let line =
+                (* On ignore les espaces *)
+                map_str (fun s -> if s = " " then "" else s)
+                (input_line cursor) 
+            in
+            let width = String.length line in
+            if width <> 9 then
+                failwith ("width = " ^ (string_of_int width) ^ " != 9") 
+            else let () =
                 String.iteri (fun j c -> match c with
                 |'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9' ->
+                    (* On convertit char en int *)
                     res.(i).(j) <- Some (int_of_string (String.make 1 c))       
-                |'-' -> () 
-                | _ -> failwith "Parsing error inconsistent"
+                |'-' -> ()
+                | c -> failwith ("invalid token " ^ (String.make 1 c))
                 ) line in fill (i + 1)
-        with End_of_file -> close_in cursor
+        with End_of_file -> 
+            close_in cursor;
+            if i < 9 then failwith ("height = " ^ (string_of_int i) ^ " != 9")
     in let () = fill 0 in res
 
 (* Affichage de sudoku en console *)
-let print_sudoku =
+let print =
     let print x = Printf.printf "%c" (match x with
-        | None -> '?'
+        | None -> '-'
         | Some y -> (string_of_int y).[0]
         )
     in
     Array.iter (fun a -> 
-        Array.iter (fun x -> print x) a; 
+        Array.iter (fun x -> print x; print_char ' ') a; 
         print_endline "")
     
